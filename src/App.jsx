70c0f052e27b4ddd10e5c68659cd6a730a6e273e
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-01 16:02:22 +0200

    Add Header, Main, and Footer components to App; update main entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
 
 export default function App() {
 
   return (
     <>
-
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+export default function Footer() {
+
+  return (
+    <>
+      <footer className="bg-warning">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <ul className="list-unstyled my-3">
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul className="list-unstyled my-3">
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul className="list-unstyled my-3">
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+                <li>Lorem.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
